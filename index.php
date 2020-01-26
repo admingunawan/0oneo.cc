e11ece158198c567d39fe4a3dfe4321262646df1
Indra Gunawan <55778242+admingunawan@users.noreply.github.com>
2020-01-26 09:01:30 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml"><head>
+<link rel="icon" type="image/png" href="http://www.zone-h.com/images/cflags/png/id.png">
+<title>Hacked by Silent's Space</title>
+</head><body style="background-color: rgb(0, 0, 0); color: rgb(0, 0, 0);" alink="#ee0000" link="#0000ee" vlink="#551a8b"><br><br><br>
+
+<div style="text-align: center;">
+<span style="color: rgb(127, 127, 126);"><br>
+<span style="font-family: monospace;">
+ <img border="0" src="https://cdn.fbsbx.com/v/t59.2708-21/84064981_628760654547229_8649956386299445248_n.gif?_nc_cat=105&amp;_nc_ohc=sUgdpWaxOJgAX9ZE73-&amp;_nc_ht=cdn.fbsbx.com&amp;oh=c30151719a88ecab68663f852478eed0&amp;oe=5E2D4707" width="400" height="400"><br>
+ <font color="gray"><pre>If I can't get you, at least I can make you happy
+</pre><br></font>
+ <span style="color: rgb(253, 253, 251);">Hacked by Silent's Space <br><br>	
+<font color="red">Xai Syndicate - Jakarta Paranoid - Bali BlackHat<br></font><font color="white"><br>Contact? trustmebrow@gmail.com</font>
+</span></span></span>
+<embed src="http://ppid.lampungbaratkab.go.id/Feast Berselancar Kami Belum Tentu (Vertical Video).mp3" type="audio/mpeg" autostart="true" loop="true" height="0" width="0">
+
+<!-- end: index --></div>
+</body></html>
